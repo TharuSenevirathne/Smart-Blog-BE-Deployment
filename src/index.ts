7646d74f1191ed054d67920662720a2f3ac32332
tharusenevirathne <tharusanduni33@gmail.com>
2025-11-24 12:05:10 +0530

    update cross origin part

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 // Middleware
 app.use(express.json())
 app.use(cors({
-    origin: [ "http://localhost:5173"], //"http://localhost:3000"
+    origin: [ "http://localhost:5173","https://smart-blog-fe-deployment.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
